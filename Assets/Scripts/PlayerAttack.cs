480af911a58cede7413a99b03d384407044fafa8
RAM-b0 <bszafraniec108@wp.pl>
2019-06-11 11:03:18 +0200

    Camera Shake

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+	private float timeBtwAttack;
+	public float startTimeBtwAttack;
+	public Transform attackPos;
+	public float attackRange;
+	public LayerMask whatIsEnemies;
+	public int damage;
+	public Animator playerAnim;
+	
+	public CameraShake cameraShake;
+	public float camShakeDuration;
+	public float camShakeMagnitude;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		if(timeBtwAttack <= 0){
+			if(Input.GetKey(KeyCode.Space)){
+				StartCoroutine( cameraShake.Shake(camShakeDuration, camShakeMagnitude) );
+				//playerAnim.SetTrigger("attack");
+				Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+				for (int i = 0; i < enemiesToDamage.Length; i++)
+				{
+					enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+				}
+			}
+			timeBtwAttack = startTimeBtwAttack;
+		}else{
+			timeBtwAttack -= Time.deltaTime;
+		}
+	}
+
+	void OnDrawGizmosSelected(){
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(attackPos.position, attackRange);
+	}
+
+}
