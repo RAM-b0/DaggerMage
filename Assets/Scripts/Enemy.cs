fc86389cd3fc12fd63400d98798429d86e03fae7
RAM-b0 <bszafraniec108@wp.pl>
2019-07-17 09:31:02 +0200

    Enemy move and shot

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,21 +6,45 @@ public class Enemy : MonoBehaviour {
 
 	public int health;
 	public float speed;
+	public float stoppingDistance;
+	public float retreatDistance;
+	public float startTimeBtwShots;
+	private float timeBtwShots;
+	public GameObject projectile;
+	public Transform player;
 
 	private Animator anim;
 
 	void Start () {
-		//anim = GetComponent<Animator>();
-		//anim.SetBool("isRunning", true);
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		
+		timeBtwShots = startTimeBtwShots;
 	}
 	
 	void Update () {
+
+		if(Vector2.Distance(transform.position, player.position) > stoppingDistance){
+			transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+		}else if( Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance ){
+			transform.position = this.transform.position;
+
+		}else if( Vector2.Distance(transform.position, player.position) < retreatDistance ){
+			transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+		}
 
 		if(health <= 0){
 			Destroy(gameObject);
 		}
 
-		transform.Translate( Vector2.left * speed * Time.deltaTime );
+		if(timeBtwShots <= 0){
+				Instantiate(projectile, transform.position, Quaternion.identity);
+				timeBtwShots = startTimeBtwShots;
+		}else{
+			timeBtwShots -= Time.deltaTime;
+		}
+
+
 	}
 
 	
